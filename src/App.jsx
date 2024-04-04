4a2a908396e307f2ae5bbb8cdd2e20d8ea60bf78
lazoliver <wilian.oliveiractt@gmail.com>
2024-04-03 23:58:49 -0300

    feat/product-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
 
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Home />
-    </>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/products/:id" Component={Product} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
